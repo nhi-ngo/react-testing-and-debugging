23d44ff75f752290689c8d3c3935400d045d33fe
nhi-ngo <nhingo.y@gmail.com>
2021-08-01 13:42:34 -0400

    snapshot testing with jest

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+test('should have App snapshot test', () => {
+	const component = renderer.create(<App />); // component constant to hold the complete UI
+	const tree = component.toJSON(); // passing it to tree constant to render it to JSON
+	expect(tree).toMatchSnapshot(); // JSON will be tested against what we have in the snapshot
 });
+
+/* press 'u' to update the snapshot */
